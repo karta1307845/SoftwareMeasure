faa2bba607c703abb7ffdc280e5f8368cee6e6a5
xiangmin <mimi04290520@gmail.com>
2019-06-15 15:49:35 +0800

    change to chinese version

--- main.php
+++ main.php
@@ -3,23 +3,39 @@
 
 // Always start this first
 session_start();
-
-if ( isset( $_SESSION['id'] ) ) {
-    // Grab user data from the database using the user_id
-    // Let them access the "logged in only" pages
-    echo "Hello! {$_SESSION['username']}";
-} else {
-    // Redirect them to the login page
-    echo "<script language='javascript' type='text/javascript'> location.href='login.php' </script>";
-}
 ?>
 
 <html>
     <head>
         <title>Main Page</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+        <link rel="stylesheet" href="css/navbar_style.css">
     </head>
     <body>
-        <h3><a href="logout.php">Logout</a></h3>        
+        <nav class="navbar navbar-expand-sm bg-light fixed-top">
+            <a class="navbar-brand" href="main.php">Logo</a>
+                <?php
+                if ( isset($_SESSION['loggedin']) ) {
+                    if ( $_SESSION['loggedin'] ) {
+                        echo '<span class="navbar-text"><h6>';
+                        echo "你好! ";
+                        echo '<u>';
+                        echo "{$_SESSION['username']}";
+                        echo '</u>';
+                        echo ", 你的ID是 {$_SESSION['id']}";
+                        echo '</h6></span>';
+                        echo '<ul class="navbar-nav ml-auto"><li class="nav-item"><a class="nav-link" href="logout.php">登出</a></li></ul>';
+                    }
+                } else {
+                    echo '<ul class="navbar-nav ml-auto"><li class="nav-item"><a class="nav-link" href="login.php">登入</a></li></ul>';
+                }
+                ?>
+        </nav>     
     </body>
 </html>
