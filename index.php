bb4af8d4fdb1955568e63c22aba3b030573f1b00
xiangmin <mimi04290520@gmail.com>
2019-06-19 10:26:49 +0800

    隨便2

--- index.php
+++ index.php
@@ -15,7 +15,15 @@ session_start();
 
         <!-- Icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/nav_style.css">
+        <link rel="stylesheet" href="css/index_style.css">
+        <script type="text/javascript">
+            $(function () {
+                $("img.dialog").click(function() {
+                    var large_image = '<img src= ' + $(this).attr("src") + '></img>';
+                    $('#dialog_large_image').html($(large_image).animate({ height: '80%', width: '100%' }, 500));
+                });
+            });
+        </script>
     </head>
     <body>
         <header>
@@ -29,29 +37,28 @@ session_start();
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="#theme">系統主題</a></li>
                 <li class="nav-item"><a class="nav-link" href="#analysis">系統分析與設計</a></li>
-                <li class="nav-item"><a class="nav-link" href="#tool">使用技術和工具</a></li>
-                <li class="nav-item"><a class="nav-link" href="#source">資料來源</a></li>
+                <li class="nav-item"><a class="nav-link" href="#source">使用技術&工具&資料來源</a></li>
                 <li class="nav-item"><a class="nav-link" href="#member">組員及分工</a></li>
                 <li class="nav-item"><a class="nav-link" href="#code">程式碼</a></li>
             </ul>
             
             <ul class="navbar-nav ml-auto">
                 <!-- Dropdown -->
-                <li class="nav-item dropdown">
+                <!--<li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">列表</a>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="action_index.php">新增專案</a>
                         <a class="dropdown-item" href="action_file.php">上傳檔案</a>
                     </div>
-                </li>
+                </li>-->
                 <?php
                 if ( isset($_SESSION['loggedin']) ) {
                     if ( $_SESSION['loggedin'] ) {
                         echo '<li class="navbar-text"><i class="fa fa-user fa-1x mr-1"></i>';
                         echo "你好! ";
-                        echo '<u>';
+                        echo '<b>';
                         echo "{$_SESSION['username']}";
-                        echo '</u></li>';
+                        echo '</b></li>';
                         echo '<li class="nav-item"><a class="nav-link" href="logout.php"><i class="fa fa-sign-out fa-1x mr-1"></i>登出</a></li>';
                     }
                 } else { 
@@ -62,37 +69,87 @@ session_start();
         </nav>
         
         <!-- section1 -->
-        <div class="container-fluid" id="theme" style="height:400px;">
+        <div class="container-fluid" id="theme">
             
         </div><hr class="section-divider">
 
         <!-- section2 -->
-        <div class="container-fluid" id="analysis" style="height:400px;">
-            
+        <div class="container-fluid" id="analysis">
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-2 text-center">
+                    <br><h4><i class="fa fa-expand-arrows-alt fa-1x mr-1"></i><b>系統架構圖</b></h4><br>
+                    <div style="border:2px grey solid; width: 210px; height: 120px;"><img class="dialog" src="./系統架構圖.png" alt="點擊圖片放大" style="width:200px; height:100px; margin-top:7px;"></div><br>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-6">
+                    <div id="dialog_large_image"></div>
+                </div>
+                <div class="col-md-2"></div>
         </div><hr class="section-divider">
 
         <!-- section3 -->
-        <div class="container-fluid" id="tool" style="height:400px;">
-            
+        <div class="container-fluid" id="source">
+            <!-- Grid row -->
+            <div class="row">
+                <div class="col-md-2"></div>
+                <!-- Grid column -->
+                <div class="col-md-3">
+
+                    <!-- Content -->
+                    <br><h4 class="font-weight-bold"><i class="fa fa-cog fa-1x mr-1"></i>使用技術</h4>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+
+                    <p>HTML5</p>
+                    <p>CSS 3</p>
+                    <p>JavaScript</p>
+                    <p>jQuery</p>
+                    <p>PHP</p>
+                    <p><a href="https://getbootstrap.com/" target="_blank">Bootstrap 4</a></p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3">
+
+                    <!-- Links -->
+                    <br><h4 class="font-weight-bold"><i class="fa fa-wrench fa-1x mr-1"></i>使用工具</h4>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p><a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a></p>
+                    <p>Chrome</p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4">
+
+                    <!-- Links -->
+                    <br><h4 class="font-weight-bold"><i class="fa fa-map-pin fa-1x mr-1"></i>資料來源</h4>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p><a href="https://www.w3schools.com/default.asp" target="_blank">W3School</a></p>
+                    <p><a href="https://fontawesome.com/" target="_blank">Font Awesome</a></p>
+                    <br><br>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
         </div><hr class="section-divider">
 
         <!-- section4 -->
-        <div class="container-fluid" id="source" style="height:400px;">
+        <div class="container-fluid" id="member">
             
         </div><hr class="section-divider">
 
         <!-- section5 -->
-        <div class="container-fluid" id="member" style="height:400px;">
-            
-        </div><hr class="section-divider">
-
-        <!-- section6 -->
-        <div class="container-fluid" id="code" style="height:400px;">
+        <div class="container-fluid" id="code">
             
         </div>
 
         <!-- Copyright -->
         <div class="footer-copyright text-center py-3">&copy; 2019 Copyright</a></div>
         <!-- Copyright -->
+        
     </body>
 </html>
--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-echo "<script type='text/javascript'> alert('會員登出'); location.href='index.php'; </script>";
+echo "<script type='text/javascript'> location.href='index.php'; </script>";
 exit();
 ?>
